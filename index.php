7b0f9831fa9fbe413a59644e8760cadc9b3b2e8f
bpoinsot <benjamin@poinsot.info>
2014-05-03 12:25:23 +0200

    Updating index.php
    Adding "mb_language('uni');" & "mb_regex_encoding('UTF-8');"

--- index.php
+++ index.php
@@ -13,8 +13,10 @@ if (!extension_loaded('mbstring')) {
     die('mbstring is missing!');
 } else {
     mb_internal_encoding('UTF-8');
-    mb_http_input('UTF-8');
     mb_http_output('UTF-8');
+    mb_http_input('UTF-8');
+    mb_language('uni');
+    mb_regex_encoding('UTF-8');
 }
 // -------------------------------------------------------------------------------------------------
 
